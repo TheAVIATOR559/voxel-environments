fdfdeb72b5629e810dcaffeda0b70475aac603f6
TheAVIATOR559 <howelljacob05@gmail.com>
2021-09-15 13:00:18 -0600

    Line bug seems to be fixed???

--- Voxel Environments/Assets/Scripts/VoxelData.cs
+++ Voxel Environments/Assets/Scripts/VoxelData.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public static class VoxelData
 {
-    public static readonly int ChunkWidth = 10;
-    public static readonly int ChunkHeight = 100;
+    public static readonly int ChunkWidth = 16;
+    public static readonly int ChunkHeight = 128;
     public static readonly int WorldSizeInChunks = 10;
 
     public static int WorldSizeInVoxels
@@ -28,35 +28,35 @@ public static class VoxelData
 
     public static readonly Vector3[] voxelVerts = new Vector3[8]
     {
-        new Vector3(0,0,0), //vert 0
-        new Vector3(1,0,0), //vert 1
-        new Vector3(1,1,0), //vert 2
-        new Vector3(0,1,0), //vert 3
-        new Vector3(0,0,1), //vert 4
-        new Vector3(1,0,1), //vert 5
-        new Vector3(1,1,1), //vert 6
-        new Vector3(0,1,1) //vert 7
+        new Vector3(0,0,0), //VERT 0
+        new Vector3(1,0,0), //VERT 1
+        new Vector3(1,1,0), //VERT 2
+        new Vector3(0,1,0), //VERT 3
+        new Vector3(0,0,1), //VERT 4
+        new Vector3(1,0,1), //VERT 5
+        new Vector3(1,1,1), //VERT 6
+        new Vector3(0,1,1) //VERT 7
     };
 
     public static readonly Vector3[] faceChecks = new Vector3[6]
     {
-        new Vector3(0,0,-1),
-        new Vector3(0,0,1),
-        new Vector3(0,1,0),
-        new Vector3(0,-1,0),
-        new Vector3(-1,0,0),
-        new Vector3(1,0,0)
+        new Vector3(0, 0, -1),
+        new Vector3(0, 0, 1),
+        new Vector3(0, 1, 0),
+        new Vector3(0, -1, 0),
+        new Vector3(-1, 0, 0),
+        new Vector3(1, 0, 0)
     };
 
     //BACK, FRONT, TOP, BOTTOM, LEFT, RIGHT
     public static readonly int[,] voxelTris = new int[6, 4]
     {
-        {0,3,1,2 },//BACK FACE
-        {5,6,4,7 },//FRONT FACE
-        {3,7,2,6 },//TOP FACE
-        {1,5,0,4 },//BOTTOM FACE
-        {4,7,0,3 },//LEFT FACE
-        {1,2,5,6 }//RIGHT FACE
+        {0,3,1,2 }, //BACK FACE
+        {5,6,4,7 }, //FRONT FACE
+        {3,7,2,6 }, //TOP FACE
+        {1,5,0,4 }, //BOTTOM FACE
+        {4,7,0,3 }, //LEFT FACE
+        {1,2,5,6 } //RIGHT FACE
     };
 
     public static readonly Vector2[] voxelUVs = new Vector2[4]
